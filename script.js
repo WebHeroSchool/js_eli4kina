e8e5f86a80713ac7c84b91116c64ff70c862ef10
Elvira Snezhkova <eli4kina@gmail.com>
2021-07-02 16:51:45 +0700

    вернула весь закоментированный код обратно

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
 
-//let userName = getUserName();
-//let githubAPIURL = 'https://api.github.com/users/';
+let userName = getUserName();
+let githubAPIURL = 'https://api.github.com/users/';
 
-//if(userName !== undefined && userName.trim() != '') {
+if(userName !== undefined && userName.trim() != '') {
 
   let profileName = document.getElementById('name');
   //Почему тут profileName равен null?
   console.log(profileName);
-  /*let avatar = document.getElementById('avatar');
+  let avatar = document.getElementById('avatar');
   console.log(avatar);
-  let profileDescription = document.getElementById('profile-description');
+  let profileDescription = document.getElementById('description');
+  console.log(profileDescription);
   let profileLink = document.getElementById('link');
-  console.log(profileLink);*/
+  console.log(profileLink);
 
-/*  fetch(githubAPIURL + userName)
+ fetch(githubAPIURL + userName)
   .then(res => res.json())
   .then(json => {
     console.log('Данные из json');
@@ -22,20 +23,20 @@
     console.log(json.bio);
     console.log(json.html_url)
 
-    /*profileName.innerHTML = (json.name !== null)? json.name : "Нет данных";
+    profileName.innerHTML = (json.name !== null)? json.name : "Нет данных";
     profileDescription.innerHTML =  (json.bio !== null) ? json.bio : "Нет данных";
     profileLink.href = json.html_url;
-    avatar.src = json.avatar_url;*/
-/*  })
+    avatar.src = json.avatar_url;
+  })
   .catch(err => console.log('Информация о пользователе не доступна' + err));
 } else {
   alert('Введите не пустое имя');
-} */
+} 
 
-/*function getUserName() {
+function getUserName() {
   let searchURL = window.location.search;
   let parseURL = searchURL.split('=');
   //console.log('userName = ' + parseURL[1]);
   return parseURL[1];
- }*/
+ }
   

c98b4125b9e0874fb85c043362b595caf6c574dd
Elvira Snezhkova <eli4kina@gmail.com>
2021-07-05 14:22:38 +0700

    добавлена проверка на существование пользователя

--- script.js
+++ script.js
@@ -3,32 +3,11 @@ let userName = getUserName();
 let githubAPIURL = 'https://api.github.com/users/';
 
 if(userName !== undefined && userName.trim() != '') {
-
-  let profileName = document.getElementById('name');
-  //Почему тут profileName равен null?
-  console.log(profileName);
   let avatar = document.getElementById('avatar');
-  console.log(avatar);
   let profileDescription = document.getElementById('description');
-  console.log(profileDescription);
   let profileLink = document.getElementById('link');
-  console.log(profileLink);
 
- fetch(githubAPIURL + userName)
-  .then(res => res.json())
-  .then(json => {
-    console.log('Данные из json');
-    console.log(json.avatar_url);
-    console.log(json.name);
-    console.log(json.bio);
-    console.log(json.html_url)
-
-    profileName.innerHTML = (json.name !== null)? json.name : "Нет данных";
-    profileDescription.innerHTML =  (json.bio !== null) ? json.bio : "Нет данных";
-    profileLink.href = json.html_url;
-    avatar.src = json.avatar_url;
-  })
-  .catch(err => console.log('Информация о пользователе не доступна' + err));
+  getUser(avatar, profileDescription, profileLink);
 } else {
   alert('Введите не пустое имя');
 } 
@@ -36,7 +15,26 @@ if(userName !== undefined && userName.trim() != '') {
 function getUserName() {
   let searchURL = window.location.search;
   let parseURL = searchURL.split('=');
-  //console.log('userName = ' + parseURL[1]);
+
   return parseURL[1];
+ }
+
+function getUser (avatar, profileDescription, profileLink) {
+  fetch(githubAPIURL + userName)
+  .then(res =>  res.json())
+  .then(json => {
+
+    if(json.message != undefined) {
+      console.log(json.message);
+      profileDescription.innerHTML =  `Пользователя с ником ${userName} не существует`;
+    } else {
+      profileDescription.innerHTML =  (json.bio != null) ? json.bio : "Нет данных";
+      profileLink.href = json.html_url;
+      profileLink.innerText = (json.name != null)? json.name : "Нет данных";
+      avatar.src = json.avatar_url;
+    }
+
+  })
+  .catch(err => console.log('Информация о пользователе не доступна ' + err));
  }
   

cd9df2dd6ef1506d92394197ae48ec71acc3dcc1
Elvira Snezhkova <eli4kina@gmail.com>
2021-07-08 15:35:49 +0700

    Добавила вывод текущей даты

--- script.js
+++ script.js
@@ -6,8 +6,9 @@ if(userName !== undefined && userName.trim() != '') {
   let avatar = document.getElementById('avatar');
   let profileDescription = document.getElementById('description');
   let profileLink = document.getElementById('link');
+  let currentDateHTML = document.getElementById('date');
 
-  getUser(avatar, profileDescription, profileLink);
+  getUser(avatar, profileDescription, profileLink, currentDateHTML);
 } else {
   alert('Введите не пустое имя');
 } 
@@ -19,7 +20,7 @@ function getUserName() {
   return parseURL[1];
  }
 
-function getUser (avatar, profileDescription, profileLink) {
+function getUser (avatar, profileDescription, profileLink, currentDateHTML) {
   const load = document.getElementById('1');
   const loading = new Promise((resolve, redject) => {
     setTimeout(() => {
@@ -28,9 +29,18 @@ function getUser (avatar, profileDescription, profileLink) {
     }, 3000);
   });
 
+  const currentDate = new Promise ((resolve, redject) => {
+    setTimeout(() => {
+      let date = new Date();
+      let dateStr = date.getDate() + '.' + (date.getMonth() + 1)  + '.' + date.getFullYear();
+      resolve(currentDateHTML.innerHTML =  dateStr);
+      redject(console.log("Не удалось вычислить дату"));
+    }, 2000);
+  });
 
-  Promise.all ([loading])
-  .then (([loadPage]) => fetch(githubAPIURL + userName)) 
+
+  Promise.all ([loading, currentDate])
+  .then (([loadPage, currentDate]) => fetch(githubAPIURL + userName)) 
   .then(res =>  res.json())
   .then(json => {
 
